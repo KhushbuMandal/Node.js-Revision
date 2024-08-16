d47b9ac2d5a7f76ec534f48f00234143f4f36c52
KhushbuMandal <khushbumandal4459@gmail.com>
2024-08-16 20:26:43 +0530

    MongoDB connection CRUD Operation

--- RestAPI/server.js
+++ RestAPI/server.js
@@ -1,6 +1,7 @@
 const express = require ("express");
 const app = express();
 const fs = require("fs")
+const mongoose = require ("mongoose");
 const users = require("./MOCK_DATA.json")
 
 
@@ -10,35 +11,100 @@ app.use (express.static("public"))
 
 const PORT = 5000;
 
+//Schema
+const userSchema  = new mongoose.Schema ({
+    firstName : {
+        type : String,
+        required : true,
+    },
+    lastName : {
+        type : String,
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    jobTitle : {
+        type : String,
+    },
+    gender : {
+        type : String,
+       
+    }
+
+} , {timestamps : true})
+
+// Connection
+mongoose
+.connect("mongodb://127.0.0.1:27017/nodejs")
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log("Mongo Error" , err) );
+
+//Model
+const User = mongoose.model("User" , userSchema);
+
+
+
+
 //Routes
 app.get('/' , (req , res) => {
      res.send("This is our Home Route");
 })
 
-app.get("/users" , (req , res) => {
-    res.json(users)
+app.get("/users" ,async (req , res) => {
+    // res.setHeader("MyName" , "Khushbu")
+    // console.log(req.headers);
+    // res.json(users)
+    const all = await User.find({});
+    res.status(200).json(all);
 })
 
 
 //Nested Routing
-app.route("/users")
-    .get((req, res) => {
-        const id = Number(req.params.id);
-        const user = users.find((user) => user.id === id)
-        res.json(user);
+app.route("/users/:id")
+    .get( async (req, res) => {
+        // const id = Number(req.params.id);
+        const id = await User.findById(req.params.id);
+        // const user = users.find((user) => user.id === id)
+        
+        res.json(id);
     })
-    .post ((req, res) => {
+    .post (async (req, res) => {
         const body = req.body;
-        users.push({...body , id : users.length+1});
-        fs.writeFile("./MOCK_DATA.json" , JSON.stringify(users) , (err , data) => {
-            res.json({status : "Sucessfull"})
-        })
-    })
-    .patch(() => {
+        if (
+            !body ||
+            !body.first_name ||
+            !body.last_name ||
+            !body.email ||
+            !body.gender ||
+            !body.job_title
+        ) {
+            return res.status(400).json({msg : "All fields are required..."})
+        }
+
+        const result = await User.create({
+            firstName : body.first_name,
+            lastName : body.last_name,
+            email : body.email,
+            gender : body.gender,
+            jobTitle : body.job_title
+        });
+
+        console.log("result" , result)
+
+        return res.status(201).json({msg : "success"});
 
     })
-    .delete(() => {
+    .patch( async (req , res) => {
 
+        const user = await User.findByIdAndUpdate(req.params.id , {lastName : "Choudary"})
+        res.json(user);
+
+    })
+    .delete( async (req , res) => {
+        const user = await User.findByIdAndDelete(req.params.id);
+        res.json(user)
     })
 
 
